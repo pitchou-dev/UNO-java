cfb9f1a3db76231e9e94286506a2cf24c3fcc4fe
hazmonyob <hazemdjennane@gmail.com>
2025-11-24 17:29:57 +0100

    fixed a menu error

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,7 @@
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("1. Start Game" + "\n" +  "2. Exit");
+        System.out.println("""
+                           1. Start Game
+                           2. Exit""");
     }
 }
