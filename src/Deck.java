3af0a3c315fe6578d1475c90b6effc62444a46b5
Sofianeaoi <aouadi.sofiane2006@gmail.com>
2025-11-24 22:53:04 +0100

    class deck with emty methodes

---
+++ src/Deck.java
@@ -0,0 +1,13 @@
+public class Deck {
+
+    public List<Card> shuffle() {
+        // implementation
+    };
+    public card drawCard() {
+        // implementation
+    };
+public boolean isEmpty() {
+        // implementation
+    }
+        
+}
