6b4257915c0897ce4dc04639981930e0d20eb9ba
hazmonyob <hazemdjennane@gmail.com>
2025-11-24 23:11:58 +0100

    fixed something in menu java 8 incompatibility

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,6 @@
 public class Menu {
     public static void main(String[] args) {
-        System.out.println("""
-                           1. Start Game
-                           2. Exit""");
+        System.out.println("1. Start Game\n" +
+                   "2. Exit");
     }
 }
