9cf151459d21fbecc81d7034115aac3fe8c60d59
hazmonyob <hazemdjennane@gmail.com>
2025-11-25 09:14:38 +0100

    fix: Menu instenciation

--- src/Menu.java
+++ src/Menu.java
@@ -27,6 +27,8 @@ public class Menu {
     
     
     public static void main(String[] args) {
-        new Menu();
+        Menu menu = new Menu();
+        menu.getUserChoice();
+        
     }
 }
