a60a11be0a676cadfab2d0795ea9a2dfea926aea
pitchou <pitchou_fr@outlook.com>
2025-11-26 16:07:40 +0100

    feat: add initDeck method

--- src/Deck.java
+++ src/Deck.java
@@ -1,18 +1,40 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Deck {
 
-    private Card cards[];
+    private List<Card> cards;
+
+    public void initDeck() {
+        this.cards = new ArrayList<>();
+        for (Color color : Color.values()) {
+            
+            if(color == Color.BLACK) {
+                continue;
+            }
+
+            NumberCard numberCard_0 = new NumberCard(color, 0);
+            this.cards.addLast(numberCard_0);
+            
+            for (int i = 1; i <= 9; i++) {
+                NumberCard numberCard = new NumberCard(color, i);
+                this.cards.addLast(numberCard);
+                NumberCard secondNumberCard = new NumberCard(color, i);
+                this.cards.addLast(secondNumberCard);
+            }
+        }
+    }
 
     public void shuffle() {
         // implementation
     };
 
     public Card drawCard() {
-        //temporary:
-        return this.cards[0];
-    };
+        return this.cards.removeFirst();
+    }
+
     public boolean isEmpty() {
-        //temporary:
-        return false;
+        return this.cards.isEmpty();
     }
         
 }
