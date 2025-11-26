f4c8974d755bfa48eff129afb7e00c4b9260ef43
pitchou <pitchou_fr@outlook.com>
2025-11-26 16:02:07 +0100

    feat: add Card constructor and getter

--- src/Card.java
+++ src/Card.java
@@ -1,3 +1,12 @@
-public class Card {
+abstract public class Card {
     private Color color;
+
+    public Card(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return this.color;
+    }
+
 }
