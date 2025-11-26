add730e4306348e94299e6305411e44c328007aa
hazmonyob <hazemdjennane@gmail.com>
2025-11-26 18:48:06 +0100

    feat:full demo class for tp 2

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,15 @@
 public class Demo {
     public static void main(String[] args) {
-        
-    }
+        Deck deck = new Deck();
+        deck.shuffle();
+        Player player1 = new Player();
+        player1.setName("Ahmed");
+        player1.drawCard(deck);
+        System.out.println(player1.getName() + " has " + player1.getHand() + " card(s) in hand.");
+        player1.displayHand();
+        player1.drawCard(deck);
+        player1.drawCard(deck);
+        System.out.println(player1.getName() + " has " + player1.getHand() + " card(s) in hand.");
+        player1.displayHand();
+    }  //the demo for tp 2 
 }
