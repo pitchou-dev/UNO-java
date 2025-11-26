214f721540c735d24e4923134fbc7ecdecbdcf82
pitchou <pitchou_fr@outlook.com>
2025-11-26 16:03:31 +0100

    feat: add NumberCard constructor and value getter

--- src/NumberCard.java
+++ src/NumberCard.java
@@ -1,3 +1,12 @@
 public class NumberCard extends Card {
     private int value;
+
+    public NumberCard(Color color, int value) {
+        super(color);
+        this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
 }
