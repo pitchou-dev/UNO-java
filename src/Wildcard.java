cee25f981a3c57d56c017fc317f2c82655e2d615
Sofianeaoi <aouadi.sofiane2006@gmail.com>
2025-12-04 10:03:10 +0100

    Add Wildcard class

---
+++ src/Wildcard.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Wildcard extends Card implements Actionable   {
+    private Wildtype wildcard;
+    public Wildcard(Wildtype wildcard) {
+        super(Color.BLACK);
+        this.wildcard=wildcard;
+
+    
+    }
+    public Color Chosencolor(){
+        System.out.println("Choose a color:1-Red,2-Yellow,3-Blue,4-Green");
+        Scanner scan = new Scanner(System.in);
+        int Choice= scan.nextInt();
+        Color newcolor;
+        newcolor= Color.BLACK;
+    
+        switch(Choice){
+            case 1:
+                newcolor= Color.RED;
+                
+            break;
+            case 2:
+                newcolor= Color.YELLOW;
+            break;
+            case 3:
+                newcolor=  Color.BLUE;
+            break;
+            case 4:
+                newcolor=  Color.GREEN;
+            break;
+            
+            default:
+                System.out.println("Invalid Choice, Try again");
+
+                Chosencolor();
+            break;
+
+
+        }
+        return newcolor;
+    }
+    @Override
+
+    public void Applyeffect(Game game){
+        game.setcurrentColor(Chosencolor());
+        if( wildcard== Wildtype.DRAW_FOUR){
+            System.out.println("Next player draws four cards");
+            game.nextPlayer();
+            for(int i=0;i<4;i++){
+                game.getPlayer(game.getcurrentPlayerIndex()).drawCard(game.getDeck());
+            } 
+        }
+    }
+
+}
+
