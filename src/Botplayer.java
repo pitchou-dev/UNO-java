79372116e7bad3e8d78aa399af04f7243e00b890
pitchou <pitchou_fr@outlook.com>
2025-12-22 14:48:49 +0100

    fix: bot setName method

--- src/Botplayer.java
+++ src/Botplayer.java
@@ -1,43 +1,36 @@
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Botplayer extends Player {
-    private int i=random(0, super.getHandSize()-1);
+
+    private static int numberOfBots = 0;
+    private String botName;
+    private int i = random(0, super.getHandSize()-1);
+
     public int random(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
+
     @Override
     public void setName() {
-        int y = 0;
-        y++;
-        String botName = "BotPlayer" + y; 
+        numberOfBots++;
+        botName = "BotPlayer" + numberOfBots;
     }
-    public void drawCard(Deck deck) {
-        super.drawCard(deck);
+
+    @Override
+    public String getName() {
+        return botName;
     }
+
+    @Override
     public Card playCard() {
         System.out.println(getName() + " played: " + getHand().get(i));
         return getHand().remove(i);
     }
+
+    @Override
     public void sayUno() {
         super.sayUno();
     }
-    public int getHandSize() {
-        return super.getHandSize();
-    }
-    public void displayHand() {
-        super.displayHand();
-    }
-    public ArrayList<Card> getHand() {
-        return super.getHand();
-    }
-    public boolean CanPlayerPlay(Card topCard, Color currentColor) {
-        return super.CanPlayerPlay(topCard, currentColor);
-    }
-    
-
-    
-    
     
 }
